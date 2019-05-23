Added Softmax Activation and correct hot en-/decoding

CNNsolver/evaluation.py
@@ -1,1 +1,18 @@
-asd
+import numpy
+import scipy.misc
+import pickle
+
+def visual_evaluation():
+    # pickle_out = open("model/dict.pickle","wb")
+    # pickle.dump(res, pickle_out)
+    # pickle_out.close()
+
+    # pickle_in = open("model/dict.pickle","rb")
+    # res = pickle.load(pickle_in)
+    #
+    # print(res[0].seg_data)
+    #
+    # res = numpy.asarray(res)
+    # for i in range(0, len(res[0])):
+    #     fpath = "model/" + "bild" + str(i) + ".png"
+    #     scipy.misc.imsave(str(fpath), res[0][i])

CNNsolver/main.py
@@ -10,6 +10,7 @@ import argparse
 # Internal libraries/scripts
 import inputreader as CNNsolver_IR
 import neural_network as CNNsolver_NN
+import evaluation as CNNsolver_EV
 
 #-----------------------------------------------------#
 #                  Parse command line                 #
@@ -51,31 +52,18 @@ path_data = args.args_input
 #                    Runner code                      #
 #-----------------------------------------------------#
 # Create the Convolutional Neural Network
-#cnn_model = CNNsolver_NN.NeuralNetwork()
+cnn_model = CNNsolver_NN.NeuralNetwork()
 
 # Train the model
-#cnn_model.train(list(range(4,5)), path_data)
+cnn_model.train(list(range(4,5)), path_data)
 # Dump model
-#cnn_model.dump("model")
+cnn_model.dump("model")
 
 # Load model
-#cnn_model.load("model")
+cnn_model.load("model")
+# Predict segmentation with CNN model
+res = cnn_model.predict(list(range(4,5)), path_data)
 
 # Evaluate model
-#res = cnn_model.predict(list(range(4,5)), path_data)
-
-import pickle
-#pickle_out = open("dict.pickle","wb")
-#pickle.dump(res, pickle_out)
-#pickle_out.close()
-
-pickle_in = open("dict.pickle","rb")
-res = pickle.load(pickle_in)
-
-import numpy
-import scipy.misc
-
-res = numpy.asarray(res)
-for i in range(0, len(res[0])):
-    fpath = "bild" + str(i) + ".png"
-    scipy.misc.imsave(str(fpath), res[0][i])
+print(res)
+#CNNsolver_EV.visual_evaluation()