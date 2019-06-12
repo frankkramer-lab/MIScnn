a82c3df629c2050270d5dac839af6efb8c5ae2db
muellerdo <domey038@live.de>
2019-06-12 16:34:19 +0200

    Big update: Implementation of Tversky loss

--- CNNsolver/utils/metrics.py
+++ CNNsolver/utils/metrics.py
@@ -1,53 +1,58 @@
 #-----------------------------------------------------#
-#            Source Code was obtained from:           #
-#         https://github.com/ellisdg/3DUnetCNN        #
-#                Credit: David G Ellis                #
+#                   Library imports                   #
 #-----------------------------------------------------#
-
-from functools import partial
-
 from keras import backend as K
 
-
-def dice_coefficient(y_true, y_pred, smooth=1.):
+#-----------------------------------------------------#
+#              Standard Dice coefficient              #
+#-----------------------------------------------------#
+def dice_coefficient(y_true, y_pred, smooth=0.00001):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
-    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
-
+    return (2. * intersection + smooth) / \
+           (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
 
 def dice_coefficient_loss(y_true, y_pred):
     return -dice_coefficient(y_true, y_pred)
 
+#-----------------------------------------------------#
+#             Class-wise Dice coefficient             #
+#-----------------------------------------------------#
+def dice_classwise(y_true, y_pred, smooth=0.00001):
+    intersection = y_true * y_pred
+    intersection = K.sum(intersection, axis=[1,2,3])
+    y_true = K.sum(y_true, axis=[1,2,3])
+    y_pred = K.sum(y_pred, axis=[1,2,3])
+    dice = ((2 * intersection) + smooth) / (y_true + y_pred + smooth)
+    return dice
 
-def weighted_dice_coefficient(y_true, y_pred, axis=(-3, -2, -1), smooth=0.00001):
-    """
-    Weighted dice coefficient. Default axis assumes a "channels first" data structure
-    :param smooth:
-    :param y_true:
-    :param y_pred:
-    :param axis:
-    :return:
-    """
-    return K.mean(2. * (K.sum(y_true * y_pred,
-                              axis=axis) + smooth/2)/(K.sum(y_true,
-                                                            axis=axis) + K.sum(y_pred,
-                                                                               axis=axis) + smooth))
+def dice_classwise_loss(y_true, y_pred):
+    return -dice_classwise(y_true, y_pred)
 
+#-----------------------------------------------------#
+#                    Tversky loss                     #
+#-----------------------------------------------------#
+# Ref: salehi17, "Tversky loss function for image segmentation using 3D FCDN"
+# -> the score is computed for each class separately and then summed
+# alpha=beta=0.5 : dice coefficient
+# alpha=beta=1   : tanimoto coefficient (also known as jaccard)
+# alpha+beta=1   : produces set of F*-scores
+# implemented by E. Moebel, 06/04/18
+def tversky_loss(y_true, y_pred):
+    alpha = 0.5
+    beta  = 0.5
 
-def weighted_dice_coefficient_loss(y_true, y_pred):
-    return -weighted_dice_coefficient(y_true, y_pred)
+    ones = K.ones(K.shape(y_true))
+    p0 = y_pred      # proba that voxels are class i
+    p1 = ones-y_pred # proba that voxels are not class i
+    g0 = y_true
+    g1 = ones-y_true
 
+    num = K.sum(p0*g0, (0,1,2,3))
+    den = num + alpha*K.sum(p0*g1,(0,1,2,3)) + beta*K.sum(p1*g0,(0,1,2,3))
 
-def label_wise_dice_coefficient(y_true, y_pred, label_index):
-    return dice_coefficient(y_true[:, label_index], y_pred[:, label_index])
+    T = K.sum(num/den) # when summing over classes, T has dynamic range [0 Ncl]
 
-
-def get_label_dice_coefficient_function(label_index):
-    f = partial(label_wise_dice_coefficient, label_index=label_index)
-    f.__setattr__('__name__', 'label_{0}_dice_coef'.format(label_index))
-    return f
-
-
-dice_coef = dice_coefficient
-dice_coef_loss = dice_coefficient_loss
+    Ncl = K.cast(K.shape(y_true)[-1], 'float32')
+    return Ncl-T
