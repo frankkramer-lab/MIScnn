70936947abe2d68f96fcdf5ae639c51d4b95e510
muellerdo <domey038@live.de>
2019-08-13 14:41:16 +0200

    License update inside files

--- miscnn/data_processing/interfaces/nifti_io.py
+++ miscnn/data_processing/interfaces/nifti_io.py
@@ -1,14 +1,20 @@
 #==============================================================================#
-# Author:       Dominik Müller                                                 #
-# Copyright:    2019 IT-Infrastructure for Translational Medical Research,     #
-#               University of Augsburg                                         #
-# License:      GNU General Public License v3.0                                #
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2019 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
 #                                                                              #
-# Unless required by applicable law or agreed to in writing, software          #
-# distributed under the License is distributed on an "AS IS" BASIS,            #
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.     #
-# See the License for the specific language governing permissions and          #
-# limitations under the License.                                               #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 #==============================================================================#
 #-----------------------------------------------------#
 #                   Library imports                   #
