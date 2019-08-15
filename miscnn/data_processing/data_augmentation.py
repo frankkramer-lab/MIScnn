c7ba5ef6c3f3ff24d89062bfe95f3360abc86d6e
muellerdo <domey038@live.de>
2019-08-15 17:51:34 +0200

    Started Data Augmentation class implementation

--- miscnn/data_processing/data_augmentation.py
+++ miscnn/data_processing/data_augmentation.py
@@ -1,3 +1,37 @@
+#==============================================================================#
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2019 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#==============================================================================#
+#-----------------------------------------------------#
+#                   Library imports                   #
+#-----------------------------------------------------#
+# External libraries
+# Internal libraries/scripts
+
+#-----------------------------------------------------#
+#               Data Augmentation class               #
+#-----------------------------------------------------#
+# Class to perform diverse data augmentation techniques
+class Data_Augmentation:
+    # Class variables
+    test = "placeholder"
+    def __init__(self, data_io, cycles, types_of_dataAug):
+        print("lol")
+
 #class
 
 #init function
