43429060ff1a4d5f6e46f3cf9b85a812c1197faf
muellerdo <domey038@live.de>
2019-08-15 17:49:15 +0200

    Implemented Clipping and Normalization Preprocessing Subfunctions

---
+++ miscnn/preprocessing/subfunctions/clipping.py
@@ -0,0 +1,54 @@
+#==============================================================================#
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2019 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#==============================================================================#
+#-----------------------------------------------------#
+#                   Library imports                   #
+#-----------------------------------------------------#
+# External libraries
+from abc import ABC, abstractmethod
+import numpy as np
+# Internal libraries/scripts
+from miscnn.preprocessing.subfunctions.abstract_subfunction import Abstract_Subfunction
+
+#-----------------------------------------------------#
+#             Subfunction class: Clipping             #
+#-----------------------------------------------------#
+""" A Clipping Subfunction class which can be used for clipping intensity pixel values on a certain range.
+
+Methods:
+    __init__                Object creation function
+    transform:              Clipping the imaging data
+"""
+class Clipping(Abstract_Subfunction):
+    #---------------------------------------------#
+    #                Initialization               #
+    #---------------------------------------------#
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
+
+    #---------------------------------------------#
+    #                  Transform                  #
+    #---------------------------------------------#
+    def transform(self, sample, training=True):
+        # Access image
+        image = sample.img_data
+        # Perform clipping
+        image_clipped = np.clip(image, self.min, self.max)
+        # Update the sample with the normalized image
+        sample.img_data = image_clipped
