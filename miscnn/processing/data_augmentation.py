bdff86a94fcb27a1a5351e318308d8ecd5e9edb3
muellerdo <domey038@live.de>
2019-11-21 15:02:55 +0100

    Add credits to batchgenerators package in Data Augmentation class

--- miscnn/processing/data_augmentation.py
+++ miscnn/processing/data_augmentation.py
@@ -17,6 +17,9 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 #==============================================================================#
 #-----------------------------------------------------#
+#  Data Augmentation utilizes the following package:  #
+#     https://github.com/MIC-DKFZ/batchgenerators     #
+#-----------------------------------------------------#
 #                   Library imports                   #
 #-----------------------------------------------------#
 # External libraries
