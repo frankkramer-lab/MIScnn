98c4f3d566cc84d0224decb46acecddb3ab27c99
muellerdo <domey038@live.de>
2019-12-13 14:25:46 +0100

    Update to version 0.26

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("docs/README.PyPI.md", "r") as fh:
 
 setup(
    name='miscnn',
-   version='0.25',
+   version='0.26',
    description='Framework for Medical Image Segmentation with Convolutional Neural Networks and Deep Learning',
    url='https://github.com/frankkramer-lab/MIScnn',
    author='Dominik Müller',
