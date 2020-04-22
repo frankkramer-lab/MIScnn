72e80669b1b1bd6fa281b9aaba682dcb44fc2c27
muellerdo <domey038@live.de>
2020-04-22 15:07:13 +0200

    Update copyright to 2020

--- miscnn/data_loading/interfaces/__init__.py
+++ miscnn/data_loading/interfaces/__init__.py
@@ -1,2 +1,20 @@
+#==============================================================================#
+#  Author:       Dominik Müller                                                #
+#  Copyright:    2020 IT-Infrastructure for Translational Medical Research,    #
+#                University of Augsburg                                        #
+#                                                                              #
+#  This program is free software: you can redistribute it and/or modify        #
+#  it under the terms of the GNU General Public License as published by        #
+#  the Free Software Foundation, either version 3 of the License, or           #
+#  (at your option) any later version.                                         #
+#                                                                              #
+#  This program is distributed in the hope that it will be useful,             #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+#  GNU General Public License for more details.                                #
+#                                                                              #
+#  You should have received a copy of the GNU General Public License           #
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+#==============================================================================#
 from miscnn.data_loading.interfaces.nifti_io import NIFTI_interface
 from miscnn.data_loading.interfaces.dictionary_io import Dictionary_interface
--- miscnn/neural_network/model.py
+++ miscnn/neural_network/model.py
@@ -1,6 +1,6 @@
 #==============================================================================#
 #  Author:       Dominik Müller                                                #
-#  Copyright:    2019 IT-Infrastructure for Translational Medical Research,    #
+#  Copyright:    2020 IT-Infrastructure for Translational Medical Research,    #
 #                University of Augsburg                                        #
 #                                                                              #
 #  This program is free software: you can redistribute it and/or modify        #
