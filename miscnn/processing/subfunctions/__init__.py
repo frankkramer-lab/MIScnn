75432a6ec710a7580b96ed6c24a6a7668c1c94ed
muellerdo <domey038@live.de>
2020-04-28 22:39:34 +0200

    Added padding to subfunction __init__

--- miscnn/processing/subfunctions/__init__.py
+++ miscnn/processing/subfunctions/__init__.py
@@ -20,3 +20,4 @@ from miscnn.processing.subfunctions.clipping import Clipping
 from miscnn.processing.subfunctions.normalization import Normalization
 from miscnn.processing.subfunctions.resampling import Resampling
 from miscnn.processing.subfunctions.resize import Resize
+from miscnn.processing.subfunctions.padding import Padding
