a54c7c6d800b539a5c65fe6b6ad5adc8fb5b708f
muellerdo <domey038@live.de>
2020-06-08 22:49:10 +0200

    Implemented unittesting for Sample Loading

--- tests/test_dataio.py
+++ tests/test_dataio.py
@@ -25,7 +25,7 @@ import tempfile
 import os
 import numpy as np
 #Internal libraries
-from miscnn import Data_IO
+from miscnn import Data_IO as DataIO
 from miscnn.data_loading.interfaces import Dictionary_interface
 
 #-----------------------------------------------------#
@@ -37,15 +37,18 @@ class Data_IO(unittest.TestCase):
     def setUpClass(self):
         # Create imgaging and segmentation data set
         np.random.seed(1234)
-        dataset = dict()
+        self.dataset = dict()
         for i in range(0, 10):
             img = np.random.rand(16, 16, 16) * 255
             self.img = img.astype(int)
             seg = np.random.rand(16, 16, 16) * 3
             self.seg = seg.astype(int)
-            dataset["TEST.sample_" + str(i)] = (img, seg)
+            if i == 3 : sample = (self.img, self.seg, self.seg)
+            elif i == 5 : sample = (self.img, None, self.seg)
+            else : sample = (self.img, self.seg)
+            self.dataset["TEST.sample_" + str(i)] = sample
         # Initialize Dictionary IO Interface
-        self.io_interface = Dictionary_interface(dataset)
+        self.io_interface = Dictionary_interface(self.dataset)
         # Initialize temporary directory
         self.tmp_batches = tempfile.TemporaryDirectory(prefix="tmp.miscnn.",
                                                        suffix=".batches")
@@ -60,7 +63,69 @@ class Data_IO(unittest.TestCase):
     #-------------------------------------------------#
     # Class Creation
     def test_BASE_create(self):
-        interface = Image_interface()
+        data_io = DataIO(self.io_interface, input_path="", output_path="",
+                         batch_path=self.tmp_batches, delete_batchDir=False)
+
+
+    # Obtain sample list
+    def test_BASE_getSampleList(self):
+        data_io = DataIO(self.io_interface, input_path="", output_path="",
+                         batch_path=self.tmp_batches, delete_batchDir=False)
+        sample_list = data_io.get_indiceslist()
+        self.assertEqual(len(sample_list), 10)
+        self.assertIn("TEST.sample_0", sample_list)
+
+    # Sample Loader - Imaging
+    def test_BASE_SampleLoader_Imaging(self):
+        data_io = DataIO(self.io_interface, input_path="", output_path="",
+                         batch_path=self.tmp_batches, delete_batchDir=False)
+        sample = data_io.sample_loader("TEST.sample_0", backup=False,
+                                       load_seg=False, load_pred=False)
+        self.assertTrue(np.array_equal(np.reshape(sample.img_data, (16,16,16)),
+                                       self.dataset["TEST.sample_0"][0]))
+        self.assertEqual(sample.img_data.shape, (16, 16, 16, 1))
+
+    # Sample Loader - Segmentation
+    def test_BASE_SampleLoader_Segmentation(self):
+        data_io = DataIO(self.io_interface, input_path="", output_path="",
+                         batch_path=self.tmp_batches, delete_batchDir=False)
+        sample = data_io.sample_loader("TEST.sample_0", backup=False,
+                                       load_seg=True, load_pred=False)
+        self.assertTrue(np.array_equal(np.reshape(sample.seg_data, (16,16,16)),
+                                       self.dataset["TEST.sample_0"][1]))
+        self.assertEqual(sample.seg_data.shape, (16, 16, 16, 1))
+        self.assertIsNotNone(sample.img_data)
+        self.assertIsNone(sample.pred_data)
+        with self.assertRaises(Exception):
+            sample = data_io.sample_loader("TEST.sample_5", backup=False,
+                                           load_seg=True, load_pred=False)
+
+    # Sample Loader - Prediction
+    def test_BASE_SampleLoader_Prediction(self):
+        data_io = DataIO(self.io_interface, input_path="", output_path="",
+                         batch_path=self.tmp_batches, delete_batchDir=False)
+        sample = data_io.sample_loader("TEST.sample_5", backup=False,
+                                       load_seg=False, load_pred=True)
+        self.assertTrue(np.array_equal(np.reshape(sample.pred_data, (16,16,16)),
+                                       self.dataset["TEST.sample_5"][2]))
+        self.assertEqual(sample.pred_data.shape, (16, 16, 16, 1))
+        self.assertIsNotNone(sample.img_data)
+        self.assertIsNone(sample.seg_data)
+        with self.assertRaises(Exception):
+            sample = data_io.sample_loader("TEST.sample_0", backup=False,
+                                           load_seg=False, load_pred=True)
+
+    # Sample Loader - Complete
+    def test_BASE_SampleLoader_Combined(self):
+        data_io = DataIO(self.io_interface, input_path="", output_path="",
+                         batch_path=self.tmp_batches, delete_batchDir=False)
+        sample = data_io.sample_loader("TEST.sample_3", backup=False,
+                                       load_seg=True, load_pred=True)
+        self.assertIsNotNone(sample.img_data)
+        self.assertIsNotNone(sample.seg_data)
+        self.assertIsNotNone(sample.pred_data)
+        self.assertEqual(sample.img_data.shape, sample.seg_data.shape)
+        self.assertEqual(sample.seg_data.shape, sample.pred_data.shape)
 
     # #-------------------------------------------------#
     # #                 Batch Management                #
