e10c64d97db76f37fecc6237c68dd3997394ea38
muellerdo <domey038@live.de>
2020-06-08 21:43:19 +0200

    Started working on Data IO unittests

--- tests/test_dataio.py
+++ tests/test_dataio.py
@@ -16,37 +16,68 @@
 #  You should have received a copy of the GNU General Public License           #
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 #==============================================================================#
+#-----------------------------------------------------#
+#                   Library imports                   #
+#-----------------------------------------------------#
+#External libraries
+import unittest
+import tempfile
+import os
+import numpy as np
+#Internal libraries
+from miscnn import Data_IO
+from miscnn.data_loading.interfaces import Dictionary_interface
 
+#-----------------------------------------------------#
+#                  Unittest: Data IO                  #
+#-----------------------------------------------------#
+class Data_IO(unittest.TestCase):
+    # Create random imaging and segmentation data
+    @classmethod
+    def setUpClass(self):
+        # Create imgaging and segmentation data set
+        np.random.seed(1234)
+        dataset = dict()
+        for i in range(0, 10):
+            img = np.random.rand(16, 16, 16) * 255
+            self.img = img.astype(int)
+            seg = np.random.rand(16, 16, 16) * 3
+            self.seg = seg.astype(int)
+            dataset["TEST.sample_" + str(i)] = (img, seg)
+        # Initialize Dictionary IO Interface
+        self.io_interface = Dictionary_interface(dataset)
+        # Initialize temporary directory
+        self.tmp_batches = tempfile.TemporaryDirectory(prefix="tmp.miscnn.",
+                                                       suffix=".batches")
 
-# # Initialize Data IO Interface for NIfTI data
-# interface = NIFTI_interface(channels=1, classes=3)
-#
-# # Create Data IO object to load and write samples in the file structure
-# data_io = Data_IO(interface, path_data, delete_batchDir=True)
-#
-# # Access all available samples in our file structure
-# sample_list = data_io.get_indiceslist()
-# sample_list.sort()
-#
-# # Print out the sample list
-# print("Sample list:", sample_list)
-#
-# # Now let's load each sample and obtain collect diverse information from them
-# sample_data = {}
-# for index in tqdm(sample_list):
-#     # Sample loading
-#     sample = data_io.sample_loader(index, load_seg=True)
-#     # Create an empty list for the current asmple in our data dictionary
-#     sample_data[index] = []
-#     # Store the volume shape
-#     sample_data[index].append(sample.img_data.shape)
-#     # Identify minimum and maximum volume intensity
-#     sample_data[index].append(sample.img_data.min())
-#     sample_data[index].append(sample.img_data.max())
-#     # Store voxel spacing
-#     sample_data[index].append(sample.details["spacing"])
-#     # Identify and store class distribution
-#     unique_data, unique_counts = np.unique(sample.seg_data, return_counts=True)
-#     class_freq = unique_counts / np.sum(unique_counts)
-#     class_freq = np.around(class_freq, decimals=6)
-#     sample_data[index].append(tuple(class_freq))
+    # Delete all temporary files
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+    #-------------------------------------------------#
+    #                Base Functionality               #
+    #-------------------------------------------------#
+    # Class Creation
+    def test_BASE_create(self):
+        interface = Image_interface()
+
+    # #-------------------------------------------------#
+    # #                 Batch Management                #
+    # #-------------------------------------------------#
+    # # Class Creation
+    # def test_BASE_create(self):
+    #     interface = Image_interface()
+    #
+    # #-------------------------------------------------#
+    # #                Evaluation Backup                #
+    # #-------------------------------------------------#
+    # # Class Creation
+    # def test_EVALUATION_create(self):
+    #     interface = Image_interface()
+
+#-----------------------------------------------------#
+#               Unittest: Main Function               #
+#-----------------------------------------------------#
+if __name__ == '__main__':
+    unittest.main()
