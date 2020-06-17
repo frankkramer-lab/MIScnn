26375273949b80f42638d95dfeacfc3d6fe144d0
muellerdo <domey038@live.de>
2020-06-17 10:41:25 +0200

    Started working on unittesting for the Neural Network class

--- tests/test_neuralnetwork.py
+++ tests/test_neuralnetwork.py
@@ -16,27 +16,93 @@
 #  You should have received a copy of the GNU General Public License           #
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 #==============================================================================#
+#-----------------------------------------------------#
+#                   Library imports                   #
+#-----------------------------------------------------#
+#External libraries
+import unittest
+import tempfile
+import os
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+#Internal libraries
+from miscnn import Data_IO, Preprocessor
+from miscnn.data_loading.interfaces import Dictionary_interface
+
+#-----------------------------------------------------#
+#              Unittest: Neural Network               #
+#-----------------------------------------------------#
+class PreprocessorTEST(unittest.TestCase):
+    # Create random imaging and segmentation data
+    @classmethod
+    def setUpClass(self):
+        np.random.seed(1234)
+        # Create 2D imgaging and segmentation data set
+        self.dataset2D = dict()
+        for i in range(0, 10):
+            img = np.random.rand(16, 16) * 255
+            self.img = img.astype(int)
+            seg = np.random.rand(16, 16) * 2
+            self.seg = seg.astype(int)
+            self.dataset2D["TEST.sample_" + str(i)] = (self.img, self.seg)
+        # Initialize Dictionary IO Interface
+        io_interface2D = Dictionary_interface(self.dataset2D, classes=3,
+                                              three_dim=False)
+        # Initialize temporary directory
+        self.tmp_dir2D = tempfile.TemporaryDirectory(prefix="tmp.miscnn.")
+        tmp_batches = os.path.join(self.tmp_dir2D.name, "batches")
+        # Initialize Data IO
+        self.data_io2D = Data_IO(io_interface2D, input_path="", output_path="",
+                              batch_path=tmp_batches, delete_batchDir=False)
+        # Create 3D imgaging and segmentation data set
+        self.dataset3D = dict()
+        for i in range(0, 10):
+            img = np.random.rand(16, 16, 16) * 255
+            self.img = img.astype(int)
+            seg = np.random.rand(16, 16, 16) * 3
+            self.seg = seg.astype(int)
+            if i in range(8,10): sample = (self.img, None)
+            else : sample = (self.img, self.seg)
+            self.dataset3D["TEST.sample_" + str(i)] = sample
+        # Initialize Dictionary IO Interface
+        io_interface3D = Dictionary_interface(self.dataset3D, classes=3,
+                                              three_dim=True)
+        # Initialize temporary directory
+        self.tmp_dir3D = tempfile.TemporaryDirectory(prefix="tmp.miscnn.")
+        tmp_batches = os.path.join(self.tmp_dir3D.name, "batches")
+        # Initialize Data IO
+        self.data_io3D = Data_IO(io_interface3D, input_path="", output_path="",
+                              batch_path=tmp_batches, delete_batchDir=False)
+
+
+    # Delete all temporary files
+    @classmethod
+    def tearDownClass(self):
+        self.tmp_dir2D.cleanup()
+        self.tmp_dir3D.cleanup()
 
     #-------------------------------------------------#
     #                Base Functionality               #
     #-------------------------------------------------#
+# create
 
+# different parameter like architecture, loss, metric
 
     #-------------------------------------------------#
-    #                  Training                 #
+    #                     Training                    #
     #-------------------------------------------------#
 
-        #-------------------------------------------------#
-        #                  Prediction                 #
-        #-------------------------------------------------#
+    #-------------------------------------------------#
+    #                    Prediction                   #
+    #-------------------------------------------------#
 
-            #-------------------------------------------------#
-            #                  Validation                 #
-            #-------------------------------------------------#
+    #-------------------------------------------------#
+    #                    Validation                   #
+    #-------------------------------------------------#
 
-                #-------------------------------------------------#
-                #                  Model Storage and Loading                 #
-                #-------------------------------------------------#
+    #-------------------------------------------------#
+    #                  Model Storage and Loading                 #
+    #-------------------------------------------------#
 
 # # Initialize Data IO Interface for NIfTI data
 # interface = NIFTI_interface(channels=1, classes=3)
