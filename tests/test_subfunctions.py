94bc8f51b9a735acd8cbd6832dc10ead7bcbe2ab
Deathlymad <meyerphilip96@googlemail.com>
2021-01-15 15:15:31 +0100

    reset test change on multiprocessing

--- tests/test_subfunctions.py
+++ tests/test_subfunctions.py
@@ -32,40 +32,6 @@ from miscnn.data_loading.interfaces import Dictionary_interface
 from miscnn.data_loading.sample import Sample
 from miscnn.processing.subfunctions import *
 
-import sys
-
-walk_dir = sys.argv[1]
-
-def printDir(walk_dir):
-	print('walk_dir = ' + walk_dir)
-
-	# If your current working directory may change during script execution, it's recommended to
-	# immediately convert program arguments to an absolute path. Then the variable root below will
-	# be an absolute path as well. Example:
-	# walk_dir = os.path.abspath(walk_dir)
-	print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
-
-	for root, subdirs, files in os.walk(walk_dir):
-		print('--\nroot = ' + root)
-		list_file_path = os.path.join(root, 'my-directory-list.txt')
-		print('list_file_path = ' + list_file_path)
-
-		with open(list_file_path, 'wb') as list_file:
-			for subdir in subdirs:
-				print('\t- subdirectory ' + subdir)
-
-			for filename in files:
-				file_path = os.path.join(root, filename)
-
-				print('\t- file %s (full path: %s)' % (filename, file_path))
-
-				with open(file_path, 'rb') as f:
-					f_content = f.read()
-					list_file.write(('The file %s contains:\n' % filename).encode('utf-8'))
-					list_file.write(f_content)
-					list_file.write(b'\n')
-
-
 #-----------------------------------------------------#
 #                Unittest: Subfunctions               #
 #-----------------------------------------------------#
@@ -219,7 +185,6 @@ class SubfunctionsTEST(unittest.TestCase):
         pp.mp_threads = 4
         sample_list = dataio.get_indiceslist()
         pp.run_subfunctions(sample_list, training=True)
-        printDir(tmp_batches)
         batches = pp.run(sample_list, training=True, validation=False)
         self.assertEqual(len(os.listdir(tmp_batches)), 5)
         for i in range(0, 5):
