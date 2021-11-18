fixed gradcam display. Added cerrtainty computation.

miscnn/utils/xai_visualizer.py
@@ -41,8 +41,6 @@ def display_3D(out_path, fig, axes, grad_overlay, activation_overlay = None):
     
     data = np.zeros(grad_overlay.shape[2:4])
     
-    print(grad_overlay.shape)
-    print(activation_overlay.shape)
     
     if activation_overlay is not None:
         fig.supylabel('Gradient vs Activation')
@@ -70,6 +68,18 @@ def display_3D(out_path, fig, axes, grad_overlay, activation_overlay = None):
     # Save the animation (gif)
     ani.save(out_path, writer='imagemagick', fps=30)
 
+
+def compute_certainty_score(*args):
+    s = 0
+    
+    m = 1 - np.max(args)
+    
+    for i in args:
+        n = i + m
+        s += n * n 
+    return np.sqrt(s)
+    
+vectorized_compute_certainty_score = np.vectorize(compute_certainty_score)
 
 
 def group_visualization(sample, gradcam, three_dim=True, out_dir = "vis", method="grid_display", alpha = 0.3):
@@ -124,10 +134,40 @@ def group_visualization(sample, gradcam, three_dim=True, out_dir = "vis", method
             
             display(out_path, fig, axes, grad_overlayed, activation_overlayed)
             
-    elif method == "grid_activation_delta":
+    elif method == "grid_certainty":
         if not is_activation_pred(sample.pred_data, expected_classes):
             raise ValueError("Activation Output of the neural network prediction is required in the sample.")
-        pass #compute derivative of activation and compute normalized delta to gradient attention for class
+        
+        #the certainty map distance is disported from a (hyper-)sphere to a (hyper-)cube. the intention is to weigh uncertain areas higher
+        certainty_map = vectorized_compute_certainty_score(*[sample.pred_data[..., i] for i in range(sample.pred_data.shape[-1])])
+        
+        normalized_act = sample.pred_data / np.expand_dims(np.linalg.norm(sample.pred_data, axis = -1), -1)
+        normalized_grad = gradcam / np.expand_dims(np.linalg.norm(gradcam, axis = 0) + 0.1, 0) #note the epsilon to avoid division by zero
+        
+        normalized_act = np.moveaxis(normalized_act, -1, 0)
+        
+        angle = np.sum(np.multiply(normalized_act, normalized_grad), axis = 0) #compute dot product along the vectors of the according dimension
+        
+        #angle = np.multiply(np.expand_dims(angle, -1), np.expand_dims(certainty_map, -1))
+        
+        certainty_map = vis.normalize(certainty_map)
+        angle = vis.normalize(angle)
+        
+        angle = np.stack([angle, certainty_map], axis = 0)
+        
+        angle = np.stack([angle, angle, angle], axis = -1)
+        
+        fig, axes = plt.subplots(1, 2)
+        
+        # Set up the output path for the gif
+        if not os.path.exists(out_dir):
+            os.mkdir(out_dir)
+        file_name = "visualization.certainty.case_" + str(sample.index).zfill(5) + ".gif"
+        out_path = os.path.join(out_dir, file_name)
+        
+        display(out_path, fig, axes, angle)
+        
+        pass #compute certainty score of activation map. use certainty to weight activation. then compute angle between activation and gradient vector
     elif method == "col_mapping":
         
         if not gradcam.shape[0] == 3:
@@ -192,7 +232,3 @@ def group_visualization(sample, gradcam, three_dim=True, out_dir = "vis", method
         pass #display gradients mean/maximum/sum of gradients for each pixel 
         
         
-
-#compute linear regression across all given samples attempting to correlate the provided data
-def compute_corr_p_value():
-    pass

miscnn/xai/gradcam.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from miscnn.utils.visualizer import visualize_samples
 from miscnn.utils.patch_operations import concat_matrices
 
-def generateGradients(preprocessed_input, model, cls = 1, three_dim=True, abs_w = False, posit_w = False, normalize = False):
+def generateGradients(preprocessed_input, model, cls = 1):
     # First, we create a model that maps the input image to the activations
     # of the last conv layer as well as the output predictions
     grad_model = tf.keras.models.Model(
@@ -18,12 +18,13 @@ def generateGradients(preprocessed_input, model, cls = 1, three_dim=True, abs_w 
         last_conv_layer_output, preds = grad_model(preprocessed_input)
         A = preds[..., cls]
 
-        # This is the gradient of the output neuron (top predicted or chosen)
-        # with regard to the output feature map of the last conv layer
+    # This is the gradient of the output neuron (top predicted or chosen)
+    # with regard to the output feature map of the last conv layer
     grads = tape.gradient(A, last_conv_layer_output)
     
-    A, grads = A[0, :], grads[0]
+    return A[0, :], grads[0]
 
+def apply_alpha(A, grads, three_dim=True, abs_w = False, posit_w = False, normalize = False):
     """Defines a matrix of alpha^k_c. Each alpha^k_c denotes importance (weights) of a feature map A^k for class c.
     If abs_w=True, absolute values of the matrix are processed and returned as weights.
     If posit_w=True, ReLU is applied to the matrix."""
@@ -50,7 +51,6 @@ def generateGradients(preprocessed_input, model, cls = 1, three_dim=True, abs_w 
 
 def computeGradientHeatmap(sample_list, model, cls = 1, three_dim=True, abs_w = False, posit_w = False, normalize = False, out_dir = "vis", progress = False, return_data=False):
     
-    
     pp = model.preprocessor
     skip_blanks = pp.patchwise_skip_blanks
     pp.patchwise_skip_blanks = False
@@ -72,20 +72,25 @@ def computeGradientHeatmap(sample_list, model, cls = 1, three_dim=True, abs_w = 
         ret = []
     
     for index, sample in zip(sample_list, preprocessed_input):
-        p = []
+        p_A = []
+        p_g = []
         
         for patch in sample:
-            patchPred = generateGradients(patch, model, cls, three_dim, abs_w, posit_w, normalize)
-            p.append(patchPred)
+            patchA, patchGrad = generateGradients(patch, model, cls)
+            p_A.append(patchA)
+            p_g.append(patchGrad)
             if progress:
                 pbar.update(1)
         
-        npp = np.asarray(p)
+        np_a = np.asarray(p_A)
+        np_g = np.asarray(p_g)
         
-        if npp.min() < 0:
-            npp -= npp.min() #account for negative weights
+        pre_shp = pp.cache["shape_" + str(index)]
+        A = pp.postprocessing(pp.cache[index], np_a, activation_output=True)
+        pp.cache["shape_" + str(index)] = pre_shp
+        grad = pp.postprocessing(pp.cache.pop(index), np_g, activation_output=True)
         
-        grad_img = pp.postprocessing(pp.cache.pop(index), npp, activation_output=True)
+        grad_img = apply_alpha(A, grad, three_dim, abs_w, posit_w, normalize)
         
         if not return_data:
             s = pp.data_io.sample_loader(index, False, False)
@@ -123,22 +128,27 @@ def computeMulticlassGradients(sample_list, model, class_list=[0, 1], three_dim=
         grad_img = np.zeros((len(class_list),) + pp.cache["shape_" + str(index)])
         
         for cls in class_list:
-            p = []
+            p_A = []
+            p_g = []
             
             for patch in sample:
-                patchPred = generateGradients(patch, model, cls, three_dim, abs_w, posit_w, normalize)
-                p.append(patchPred)
+                patchA, patchGrad = generateGradients(patch, model, cls)
+                p_A.append(patchA)
+                p_g.append(patchGrad)
                 if progress:
                     pbar.update(1)
             
-            npp = np.asarray(p)
+            np_a = np.asarray(p_A)
+            np_g = np.asarray(p_g)
             
-            if npp.min() < 0:
-                npp -= npp.min() #account for negative weights
             pre_shp = pp.cache["shape_" + str(index)]
-            grad_img[cls] = pp.postprocessing(pp.cache[index], npp, activation_output=True)
+            A = pp.postprocessing(pp.cache[index], np_a, activation_output=True)
             pp.cache["shape_" + str(index)] = pre_shp
-        
+            grad = pp.postprocessing(pp.cache[index], np_g, activation_output=True)
+            pp.cache["shape_" + str(index)] = pre_shp
+            
+            grad_img[cls] = apply_alpha(A, grad, three_dim, abs_w, posit_w, normalize)
+            
         pp.cache.pop("shape_" + str(index))
         pp.cache.pop(index)
         